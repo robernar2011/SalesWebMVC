7c0cff338532faecd0bc7538f03d1e55e99c81a3
robernar2011 <robernar2011@gmail.com>
2021-09-10 18:04:09 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SalesWebMvc.Models
+namespace SalesWebMvc.Models.ViewModels
 {
     public class ErrorViewModel
     {
@@ -8,4 +8,4 @@ namespace SalesWebMvc.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
-}
+} 
