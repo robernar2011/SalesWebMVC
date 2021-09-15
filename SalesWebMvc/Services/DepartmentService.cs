812733d335b24c92deeaf391b54a0727d4bee5fa
robernar2011 <robernar2011@gmail.com>
2021-09-15 12:03:58 -0300

    Delete seller

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
     public class DepartmentService
     {
+        private readonly SalesWebMvcContext _context;
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
     }
 }
